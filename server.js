df5f92b56faa27303b7cf50f08e23b5bb7389487
Niraj Kadam <130681435+Nirajkadam18@users.noreply.github.com>
2025-04-10 00:56:25 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const jobRoutes = require("./routes/jobRoutes");
+app.use("/api", jobRoutes);
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+    res.send("Student Job Tracker API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
